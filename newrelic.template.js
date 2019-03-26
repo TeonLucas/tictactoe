253bfa6456e6e8d5b3496e2fd339799840ef7478
David Santia <davidsantia.morris@gmail.com>
2019-03-26 11:47:45 -0700

    Fix license template name

--- newrelic.template.js
+++ newrelic.template.js
@@ -9,7 +9,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['express-squirrelly'],
+  app_name: ['express-react-app'],
   /**
    * Your New Relic license key.
    */
