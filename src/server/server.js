e89e9a9502fa18d19b872f1c8b08b0a6d4cbbf8e
David Santia <davidsantia.morris@gmail.com>
2019-03-26 18:22:12 -0700

    Change template engine to doT

--- src/server/server.js
+++ src/server/server.js
@@ -1,25 +1,34 @@
 const newrelic = require('newrelic'),
-      express = require('express');
+      express = require('express'),
+      dot = require('express-dot-engine'),
+      path = require('path');
 
 const app = express();
 
 // Static assets
 app.use(express.static('dist'));
 
-// Squirrelly views
-app.set('views', 'dist/views');
-app.set('view engine', 'squirrelly');
+// doT views
+app.engine('dot', dot.__express);
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'dot');
 
 app.get('/', function (req, res) {
+
     res.render('index', {
-        title: 'Express Squirrelly React App',
+        title: 'Express doT React App',
         favicon: '/images/favicon.ico',
-        main: '/main.js'
+        main: '/main.js',
+        newrelic: newrelic.getBrowserTimingHeader()
     })
-})
+});
+
+app.get('/metric/:id', function (req, res) {
+    newrelic.addCustomAttribute('gameId', req.params.id);
+    res.send('{"gameId": "'+ req.params.id + '"}\n');
+});
 
 let PORT = process.env.PORT || 3000;
 let server = app.listen(PORT, function () { // This starts the server
     console.log("listening to request on port", PORT);
 });
-
