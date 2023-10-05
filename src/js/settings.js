4b7f6f0866a65cee2f1461907d45661296cbe1b1
Teon Lucas <tlucas@newrelic.com>
2023-10-04 18:10:07 -0700

    Add ajax calls to settings page

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,12 +2,52 @@ import React, { Component } from 'react';
 
 class Settings extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch("/user")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            user: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    return (
-      <div>
-        <p>Customize user profile</p>
-      </div>
-    );
+    const { error, isLoaded, user } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <p>Customize user profile</p>
+          <table>
+            <tr><td>Name</td><td><input type="text" value={user.name} /></td></tr>
+            <tr><td>Email</td><td><input type="text" value={user.email} /></td></tr>
+          </table>
+        </div>
+      );
+    }
   }
 }
 
--- src/server/server.js
+++ src/server/server.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
         title: 'Express doT React App',
         favicon: '/images/favicon.ico',
         main: '/main.js',
-        css: 'main.css',
+        css: '/main.css',
         newrelic: newrelic.getBrowserTimingHeader()
     })
 });
@@ -33,6 +33,11 @@ app.get('/metric/:id', function (req, res) {
     newrelic.addCustomAttribute('gameId', req.params.id);
     res.send('{"gameId": "'+ req.params.id + '"}\n');
 });
+
+app.get('/user', function (req, res) {
+    res.send('{"name": "Test User", "email": "test@example.com"}\n');
+});
+
 
 let PORT = process.env.PORT || 3000;
 let server = app.listen(PORT, function () { // This starts the server
